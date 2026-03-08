774eb26f89ea92976a33ea0fcd394e92efcb4735
Binh Duc Pham <binhpham_2002@Binhs-MacBook-Pro.local>
2026-03-07 17:54:19 -0800

    fix prisma client

--- dist/src/controllers/task.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { Response } from "express";
-export declare const createTask: (req: any, res: Response) => Promise<void>;
-export declare const getTasks: (req: any, res: Response) => Promise<void>;
-export declare const updateTask: (req: any, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
-export declare const deleteTask: (req: any, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
-//# sourceMappingURL=task.controller.d.ts.map
--- dist/src/middleware/role.middleware.d.ts
+++
@@ -1,3 +0,0 @@
-import { Response, NextFunction } from "express";
-export declare const requireAdmin: (req: any, res: Response, next: NextFunction) => Response<any, Record<string, any>> | undefined;
-//# sourceMappingURL=role.middleware.d.ts.map
